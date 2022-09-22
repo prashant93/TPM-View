1aa9983cae21888f8c7ea3de81965a951c7243bb
prashant93 <prashantpanigrahi93@gmail.com>
2022-09-22 23:20:52 +0530

    3 files modified

--- src/material.module.ts
+++ src/material.module.ts
@@ -1,10 +1,12 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [MatToolbarModule, MatIconModule, MatCardModule, MatMenuModule],
+  exports: [MatToolbarModule, MatIconModule, MatCardModule, MatMenuModule,DragDropModule,MatDividerModule],
 })
 export class MaterialModule {}
