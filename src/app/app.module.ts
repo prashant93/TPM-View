96eb89f97d2ccd2e1d165f7cff487baa393ac1a6
prashant93 <prashantpanigrahi93@gmail.com>
2022-09-22 22:14:17 +0530

    6 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../material.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { OffersComponent } from './offers/offers.component';
-import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule,
-  ],
+  imports: [BrowserModule, MaterialModule, BrowserAnimationsModule],
   declarations: [AppComponent, HeaderComponent, OffersComponent],
   bootstrap: [AppComponent],
 })
